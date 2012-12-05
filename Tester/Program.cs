462c80e4fb4e387858130b1fc26aac3462a58d65
Scott Williams <tocsoft@gmail.com>
2012-12-05 14:56:41 +0000

    inital checkin

---
+++ Tester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Tester
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var html = File.ReadAllText("sample.html");
+			
+			var cleanHtml = CssInliner.Inliner.InlineCssIntoHtml(html);
+			File.WriteAllText("sampleOutput.html", cleanHtml);
+		}
+	}
+}
